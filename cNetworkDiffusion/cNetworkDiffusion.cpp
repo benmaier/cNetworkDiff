64a89fcd358d9cc91f445d93d89ead8ded3e711e
Benjamin Maier <benjaminfrankmaier@gmail.com>
2017-04-05 16:29:23 +0200

    added functions for backwards compatibility

--- cNetworkDiffusion/cNetworkDiffusion.cpp
+++ cNetworkDiffusion/cNetworkDiffusion.cpp
@@ -47,28 +47,41 @@ namespace py = pybind11;
 PYBIND11_PLUGIN(cNetworkDiffusion) {
     py::module m("cNetworkDiffusion", "For fast simulations of random walks on networks.");
     
-    m.def("mgmfpt_and_mean_coverage_time", &mgmfpt_and_mean_coverage_time, "Simulates N_walker random walks on the network given as edge list and returns the mean global mean first passage time (MGMFPT) and the mean coverage time.",
+    m.def("mgmfpt_and_mean_coverage_time", &mgmfpt_and_mean_coverage_time, "Simulates N_walker random walks on the network given as edge list and returns the mean global mean first passage time (MGMFPT) and the mean coverage time. RNG is non-deterministically initialized if seed = 0.",
             py::arg("N"),
             py::arg("edge_list"),
             py::arg("coverage_ratio") = 1.0,
             py::arg("seed") = 0
             );
 
-    m.def("gmfpts_and_mean_coverage_time", &gmfpts_and_mean_coverage_time, "Simulates N_walker random walks on the network given as edge list and returns the global mean first passage time (MMFPT) per target node and the overall mean coverage time.",
+    m.def("mmfpt_and_mean_coverage_time", &mgmfpt_and_mean_coverage_time, "FOR BACKWARDS COMPATIBILITY ONLY. Simulates N_walker random walks on the network given as edge list and returns the mean global mean first passage time (MGMFPT) and the mean coverage time. RNG is non-deterministically initialized if seed = 0.",
             py::arg("N"),
             py::arg("edge_list"),
             py::arg("coverage_ratio") = 1.0,
             py::arg("seed") = 0
             );
 
-    m.def("mmfpt_and_mean_coverage_time_power_law", &mmfpt_and_mean_coverage_time_power_law, "Simulates N_walker random walks on a ring network with power-law jump distribution and returns the mean mean first passage time (MMFPT) and the mean coverage time.",
+    m.def("gmfpts_and_mean_coverage_time", &gmfpts_and_mean_coverage_time, "Simulates N_walker random walks on the network given as edge list and returns the global mean first passage time (MMFPT) per target node and the overall mean coverage time. RNG is non-deterministically initialized if seed = 0.",
+            py::arg("N"),
+            py::arg("edge_list"),
+            py::arg("coverage_ratio") = 1.0,
+            py::arg("seed") = 0
+            );
+
+    m.def("mmfpts_and_mean_coverage_time", &gmfpts_and_mean_coverage_time, "FOR BACKWARDS COMPATIBILITY ONLY. Simulates N_walker random walks on the network given as edge list and returns the global mean first passage time (MMFPT) per target node and the overall mean coverage time. RNG is non-deterministically initialized if seed = 0.",
+            py::arg("N"),
+            py::arg("edge_list"),
+            py::arg("coverage_ratio") = 1.0,
+            py::arg("seed") = 0
+            );
+    m.def("mmfpt_and_mean_coverage_time_power_law", &mmfpt_and_mean_coverage_time_power_law, "Simulates N_walker random walks on a ring network with power-law jump distribution and returns the mean mean first passage time (MMFPT) and the mean coverage time. RNG is non-deterministically initialized if seed = 0.",
             py::arg("N"),
             py::arg("alpha"),
             py::arg("coverage_ratio") = 1.0,
             py::arg("seed") = 0
             );
 
-    m.def("mmfpt_and_mean_coverage_time_power_law_k", &mmfpt_and_mean_coverage_time_power_law_k, "Simulates N_walker random walks on a ring network with power-law jump distribution where the distribution sums to k. Returns the mean mean first passage time (MMFPT) and the mean coverage time.",
+    m.def("mmfpt_and_mean_coverage_time_power_law_k", &mmfpt_and_mean_coverage_time_power_law_k, "Simulates N_walker random walks on a ring network with power-law jump distribution where the distribution sums to k. Returns the mean mean first passage time (MMFPT) and the mean coverage time. RNG is non-deterministically initialized if seed = 0.",
             py::arg("N"),
             py::arg("alpha"),
             py::arg("k"),
@@ -76,7 +89,7 @@ PYBIND11_PLUGIN(cNetworkDiffusion) {
             py::arg("seed") = 0
             );
 
-    m.def("mmfpt_and_mean_coverage_time_meanfield_MHRN", &mmfpt_and_mean_coverage_time_meanfield_MHRN, "Simulates N_walker random walks on a ring network with MHRN jump distribution where the distribution sums to k. Returns the mean mean first passage time (MMFPT) and the mean coverage time.",
+    m.def("mmfpt_and_mean_coverage_time_meanfield_MHRN", &mmfpt_and_mean_coverage_time_meanfield_MHRN, "Simulates N_walker random walks on a ring network with MHRN jump distribution where the distribution sums to k. Returns the mean mean first passage time (MMFPT) and the mean coverage time. RNG is non-deterministically initialized if seed = 0.",
             py::arg("B"),
             py::arg("L"),
             py::arg("xi"),
